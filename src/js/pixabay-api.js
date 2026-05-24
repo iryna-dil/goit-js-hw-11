f2353522530c6c0daf87ad0848016d5f48535f8d
Iryna <iradil373@gmail.com>
2026-05-24 15:48:21 +0200

    .

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY =
-  import.meta.env.VITE_PIXABAY_API_KEY || 'PASTE_YOUR_PIXABAY_API_KEY';
+const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY || '';
 
 export function getImagesByQuery(query) {
-  if (API_KEY === 'PASTE_YOUR_PIXABAY_API_KEY') {
-    return Promise.reject(new Error('Pixabay API key is not configured.'));
-  }
-
   return axios
     .get(BASE_URL, {
       params: {
